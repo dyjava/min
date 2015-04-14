5735b2aa3ebc904c26a7395327b85e55356a8174
dyong <dyong525@163.com>
2015-04-14 16:44:30 +0800

    a

--- src/main/java/com/sys/spring/min/domain/Message.java
+++ src/main/java/com/sys/spring/min/domain/Message.java
@@ -1,5 +1,133 @@
 package com.sys.spring.min.domain;
 
 public class Message {
+	
+	private String id ="" ;			//ID
+	private String name = "" ;		//企业名称
+	private String address = "" ;	//地址
+	private String person = "" ;	//法人代表
+	private String type = "" ;		//行业类别
+	private String ctime = "" ;		//注册时间
+	private String cmoney = "" ;	//注册资金
+	private String ctype = "" ;		//企业性质
+	private String cage = "" ;		//经营年限
+	private String used = "" ;		//业务范围
+	private String area = "" ;		//业务区域
+	private String web = "" ;		//公司网站
+	private String tel = "" ;		//联系电话
+	private String cz = "" ;		//传真
+	private String email = "" ;		//邮箱
+	private String weixin = "" ;	//微信
+	private String num = "" ;		//员工人数
+	private String others = "" ;	//其他补充信息
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPerson() {
+		return person;
+	}
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getCtime() {
+		return ctime;
+	}
+	public void setCtime(String ctime) {
+		this.ctime = ctime;
+	}
+	public String getCmoney() {
+		return cmoney;
+	}
+	public void setCmoney(String cmoney) {
+		this.cmoney = cmoney;
+	}
+	public String getCtype() {
+		return ctype;
+	}
+	public void setCtype(String ctype) {
+		this.ctype = ctype;
+	}
+	public String getCage() {
+		return cage;
+	}
+	public void setCage(String cage) {
+		this.cage = cage;
+	}
+	public String getUsed() {
+		return used;
+	}
+	public void setUsed(String used) {
+		this.used = used;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getWeb() {
+		return web;
+	}
+	public void setWeb(String web) {
+		this.web = web;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getCz() {
+		return cz;
+	}
+	public void setCz(String cz) {
+		this.cz = cz;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getWeixin() {
+		return weixin;
+	}
+	public void setWeixin(String weixin) {
+		this.weixin = weixin;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getOthers() {
+		return others;
+	}
+	public void setOthers(String others) {
+		this.others = others;
+	}
+
 
 }
